7fdb76908189c5b9441250ef147f1790a1087899
hiiro777 <sleeping.garden@icloud.com>
2021-08-04 19:48:24 +0900

    ユーザーのアクセス範囲の制限

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,7 @@
 class PrototypesController < ApplicationController
+  before_action :authenticate_user!, except: [:new, :create, :show, :destroy]
+  before_action :move_to_root, except: [:index, :edit]
+
   def index
     @prototypes = Prototype.all
   end
@@ -45,5 +48,11 @@ class PrototypesController < ApplicationController
   private
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_root
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
